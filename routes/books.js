7383f36e3bd63133d9cf58e235e2640ae5fd184b
slledru <slledru@msn.com>
2018-04-03 09:23:38 -0600

    post and patch

--- routes/books.js
+++ routes/books.js
@@ -10,38 +10,107 @@ const router = express.Router()
 
 router.get('/', (req, res, next) => {
   knex('books')
-    .then((rows) => {
-      const sorted = rows
-        .sort((a, b) => a.title.toUpperCase() > b.title.toUpperCase())
-        .map((record) => humps.camelizeKeys(record))
-      res.type('json')
-      res.json(sorted)
-    })
+    .then((rows) => rows.sort((a, b) => a.title.toUpperCase() > b.title.toUpperCase()))
+    .then((rows) => rows.map((record) => humps.camelizeKeys(record)))
+    .then((rows) => res.json(rows))
     .catch((err) => console.log(err))
 })
 
 router.get('/:id', (req, res, next) => {
   const { id } = req.params
+  if (id) {
+    knex(bookTable)
+      .where('id', id)
+      .then((rows) => {
+        if (rows.length > 0) {
+          res.json(humps.camelizeKeys(rows[0]))
+        }
+        else {
+          res.sendStatus(404)
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+  else {
+    res.sendStatus(404)
+  }
+})
+
+router.post('/', (req, res, next) => {
+  const { title, author, genre, description, coverUrl } = req.body
+  if (title && author && genre && description && coverUrl) {
+    const cover_url = humps.decamelizeKeys(coverUrl)
+    knex(bookTable)
+      .insert({
+        title, author, genre, description, cover_url
+      })
+      .returning('*')
+      .then((rows) => {
+        if (rows.length > 0) {
+          res.json(humps.camelizeKeys(rows[0]))
+        }
+        else {
+          res.sendStatus(404)
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+  else {
+    res.sendStatus(400)
+  }
+})
+
+router.patch('/:id', (req, res, next) => {
+  const { id } = req.params
+  const { title, author, genre, description, coverUrl } = req.body
+  if (!title) {
+    next({ status: 400, message: 'Title must not be blank' })
+  }
+  else if (!author) {
+    next({ status: 400, message: 'Author must not be blank' })
+  }
+  else if (!genre) {
+    next({ status: 400, message: 'Genre must not be blank' })
+  }
+  else if (!description) {
+    next({ status: 400, message: 'Description must not be blank' })
+  }
+  else if (!coverUrl) {
+    next({ status: 400, message: 'Cover URL must not be blank' })
+  }
+  else if (!id) {
+    next({ status: 404, message: 'Id must not be blank' })
+  }
+  else {
+    const cover_url = humps.decamelizeKeys(coverUrl)
+    knex(bookTable)
+      .update({ title, author, genre, description, cover_url })
+      .where('id', id)
+      .returning('*')
+      .then((rows) => {
+        if (rows.length > 0) {
+          res.json(humps.camelizeKeys(rows[0]))
+        }
+        else {
+          res.sendStatus(404)
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+})
+
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params
 
   knex(bookTable)
+    .del()
     .where('id', id)
+    .returning('*')
     .then((rows) => {
       res.type('json')
       res.json(humps.camelizeKeys(rows[0]))
     })
     .catch((err) => console.log(err))
-})
-
-router.post('/', (req, res, next) => {
-
-})
-
-router.patch('/:id', (req, res, next) => {
-
-})
-
-router.delete('/:id', (req, res, next) => {
-
 })
 
 module.exports = router
