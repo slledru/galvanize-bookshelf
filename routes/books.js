cdce030c7bfa56b523a0a1443491e424ab5ec97f
slledru <slledru@msn.com>
2018-04-02 21:43:42 -0600

    route for books

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,44 @@
 'use strict';
 
-const express = require('express');
+const express = require('express')
+const knex = require('../knex')
+const bookTable = 'books'
 
 // eslint-disable-next-line new-cap
-const router = express.Router();
+const router = express.Router()
 
-// YOUR CODE HERE
+router.get('/', (req, res, next) => {
+  knex('books')
+    .then((rows) => {
+      console.log(rows)
+      res.type('json')
+      res.json({ data: rows })
+    })
+    .catch((err) => console.log(err))
+})
 
-module.exports = router;
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params
+
+  knex(bookTable)
+    .where('id', id)
+    .then((rows) => {
+      res.type('json')
+      res.json({ data: rows })
+    })
+    .catch((err) => console.log(err))
+})
+
+router.post('/', (req, res, next) => {
+
+})
+
+router.patch('/:id', (req, res, next) => {
+
+})
+
+router.delete('/:id', (req, res, next) => {
+
+})
+
+module.exports = router
