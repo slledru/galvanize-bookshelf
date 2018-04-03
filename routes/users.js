85a66d9a101d54e3ebea01633316e09daebd9c87
slledru <slledru@msn.com>
2018-04-03 11:27:18 -0600

    added route for POST users

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,53 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
+const knex = require('../knex')
+const humps = require('humps')
+const boom = require('boom')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const userTable = 'users'
 
 // eslint-disable-next-line new-cap
-const router = express.Router();
+const router = express.Router()
 
-// YOUR CODE HERE
+router.post('/', (req, res, next) => {
+  const { firstName, lastName, email, password } = req.body
+  if (!firstName) {
+    next(boom.badRequest('First name must not be blank'))
+  }
+  else if (!lastName) {
+    next(boom.badRequest('Last name must not be blank'))
+  }
+  else if (!password) {
+    next(boom.badRequest('Password must not be blank'))
+  }
+  else {
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      if (err) {
+        next(boom.badImplementation())
+      }
+      else {
+        const first_name = humps.camelizeKeys(firstName)
+        const last_name = humps.camelizeKeys(lastName)
+        const hashed_password = hash
+        knex(userTable)
+          .insert({
+            first_name, last_name, email, hashed_password
+          })
+          .returning(['first_name', 'last_name', 'email', 'id'])
+          .then((rows) => {
+            if (rows.length > 0) {
+              res.json(humps.camelizeKeys(rows[0]))
+            }
+            else {
+              next(boom.notFound())
+            }
+          })
+          .catch((ex) => next(boom.badImplementation()))
+      }
+    })
+  }
+})
 
-module.exports = router;
+module.exports = router
