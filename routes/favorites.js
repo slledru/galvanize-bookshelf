c6e3405c9f5d05689010091d8fbeae1091fd62a7
slledru <slledru@msn.com>
2018-04-04 17:18:39 -0600

    create const for column names

--- routes/favorites.js
+++ routes/favorites.js
@@ -8,23 +8,25 @@ const favoriteTable = 'favorites'
 const bookTable = 'books'
 const userTable = 'users'
 
+const columns = [
+  'books.author as author',
+  'books.cover_url as coverUrl',
+  'books.created_at as createdAt',
+  'books.description as description',
+  'books.genre as genre',
+  'books.title as title',
+  'books.updated_at as updatedAt',
+  'favorites.book_id as bookId',
+  'favorites.user_id as userId',
+  'favorites.id as id'
+]
+
 // eslint-disable-next-line new-cap
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
   knex(favoriteTable)
-    .select([
-      'books.author as author',
-      'books.cover_url as coverUrl',
-      'books.created_at as createdAt',
-      'books.description as description',
-      'books.genre as genre',
-      'books.title as title',
-      'books.updated_at as updatedAt',
-      'favorites.book_id as bookId',
-      'favorites.user_id as userId',
-      'favorites.id as id'
-    ])
+    .select(columns)
     .innerJoin(bookTable, 'books.id', 'favorites.book_id')
     .then((rows) => res.json(rows))
 })
